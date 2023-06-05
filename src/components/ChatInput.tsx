d13be822eeedc62d203c101cea470a1bd0e9140b
karengomezs <karengomezsoto1@gmail.com>
2023-06-05 14:58:10 -0500

    init chat proto

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import { FC, HTMLAttributes } from "react";
+
+interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
+
+const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
+  return (
+    <div {...props} className={cn("border-t bordeer-zinc-300", className)}>
+      ChatInput
+    </div>
+  );
+};
+
+export default ChatInput;
