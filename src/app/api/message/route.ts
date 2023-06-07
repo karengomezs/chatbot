76f1de7044ceeb6073141a3b306e19cbfe444fbc
karengomezs <karengomezsoto1@gmail.com>
2023-06-06 20:52:32 -0500

    stream func

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,5 +1,5 @@
 import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt";
-import { ChatGPTMessages } from "@/lib/openai-stream";
+import { ChatGPTMessage, OpenAIStream } from "@/lib/openai-stream";
 import { MessageArraySchema } from "@/lib/validators/message";
 
 export async function POST(req: Request) {
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
 
   const parseMessages = MessageArraySchema.parse(message); //zod compara que la información que recibe es igual en estructura y tipos
 
-  const outboundMessages: ChatGPTMessages[] = parseMessages.map((message) => ({
+  const outboundMessages: ChatGPTMessage[] = parseMessages.map((message) => ({
     role: message.isUserMessage ? "user" : "system",
     content: message.text,
   }));
@@ -17,4 +17,22 @@ export async function POST(req: Request) {
     role: "system",
     content: chatbotPrompt,
   });
+
+  //esto es el objeto que debo enviarle a openai https://platform.openai.com/docs/api-reference/chat/create
+  const payload = {
+    model: "gpt-3.5-turbo",
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+
+  //esto es la respuesta de la funcion para que luego yo la consuma en el cliente
+  return new Response(stream);
 }
